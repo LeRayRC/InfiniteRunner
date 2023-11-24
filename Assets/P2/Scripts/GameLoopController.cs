cb9664c6272fc50f6b72887d3e64147c116c8b13
garciaroi <garciaroi@esat-miralles.com>
2023-11-24 11:51:47 +0100

    feat: working on levels

--- Assets/P2/Scripts/GameLoopController.cs
+++ Assets/P2/Scripts/GameLoopController.cs
@@ -9,7 +9,7 @@ public class GameLoopController : MonoBehaviour
     {
         // GameManager.instance.gameLevel = 0;
         // GameManager.instance.timeElapsedOnGame = 0.0f;
-
+        StartCoroutine(BlendingLevelMaterial(GameManager.instance.levelMaterials_[0], GameManager.instance.levelMaterials_[1]));
     }
 
     // Update is called once per frame
@@ -20,5 +20,17 @@ public class GameLoopController : MonoBehaviour
             GameManager.instance.timeElapsedOnGame = 0.0f;
             GameManager.instance.gameLevel++;
         }
+    }
+
+    IEnumerator BlendingLevelMaterial(Material prev_mat, Material next_mat){   
+        while ( prev_mat.color.a >= 0.0f)
+        {
+
+            prev_mat.color = new Color(prev_mat.color.r, prev_mat.color.g, prev_mat.color.b, prev_mat.color.a - Time.deltaTime);
+            next_mat.color = new Color(prev_mat.color.r, prev_mat.color.g, prev_mat.color.b, prev_mat.color.a + Time.deltaTime);
+        }
+        next_mat.color = new Color(prev_mat.color.r, prev_mat.color.g, prev_mat.color.b, 1.0f);
+
+        yield return new WaitForSeconds(0.1f);
     }
 }
--- Assets/P2/Scripts/GameManager.cs
+++ Assets/P2/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour{
     public GameObject enemyExplosionParticles_;
 
     public List<GameObject> bulletList_;
+    public List<Material> levelMaterials_;
     public int bulletCount_;
 
     void Awake(){
